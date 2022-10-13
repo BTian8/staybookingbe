f034d47b6373fb3c30723455a7748734eb478f8b
BTian8 <boyan.t@wustl.edu>
2022-10-13 10:27:53 -0500

    staybookingbe

---
+++ staybooking/src/main/java/com/example/staybooking/repository/ReservationRepository.java
@@ -0,0 +1,22 @@
+package com.example.staybooking.repository;
+
+import com.example.staybooking.model.Reservation;
+import com.example.staybooking.model.Stay;
+import com.example.staybooking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByGuest(User guest);
+
+    List<Reservation> findByStay(Stay stay);
+
+    Reservation findByIdAndGuest(Long id, User guest); // for deletion
+
+    List<Reservation> findByStayAndCheckoutDateAfter(Stay stay, LocalDate date);
+
+}
